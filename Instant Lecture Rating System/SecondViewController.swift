d4806aab1a182035fe8ea44a9efdd510f8a45520
Student12 <Student12@Studio12s-iMac.local>
2015-04-20 16:27:49 +0800

    Initial Commit

---
+++ Instant Lecture Rating System/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Instant Lecture Rating System
+//
+//  Created by Student12 on 4/20/15.
+//  Copyright (c) 2015 Student12. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
